70c99b219704fb22d4874d8529556e02fb32dc2d
Juan J Arosemena <40127035+jjac111@users.noreply.github.com>
2019-10-25 17:49:13 -0500

    Finished Model Training
    * All 150 models have been trained and results have been generated.
    * Additionally, created ``Results Analisys.ipynb`` for results exploration, discussion, and display.

--- source/train.py
+++ source/train.py
@@ -90,7 +90,6 @@ def train(model, train_loader, epochs, criterion, optimizer, device):
         print("Epoch: {}, Loss: {}".format(epoch, total_loss / len(train_loader)))
 
 
-## TODO: Complete the main code
 if __name__ == '__main__':
     
     # All of the model parameters and training parameters are sent as arguments
@@ -113,7 +112,7 @@ if __name__ == '__main__':
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     
-    ## TODO: Add args for the three model parameters: input_features, hidden_dim, output_dim
+   
     # Model Parameters
     parser.add_argument('--input_features', type=int, default=11, 
                          help='number of input features')
@@ -134,11 +133,7 @@ if __name__ == '__main__':
     train_loader = _get_train_data_loader(args.batch_size, args.data_dir)
 
 
-    ## --- Your code here --- ##
-    
-    ## TODO:  Build the model by passing in the input params
-    # To get params from the parser, call args.argument_name, ex. args.epochs or ards.hidden_dim
-    # Don't forget to move your model .to(device) to move to GPU , if appropriate
+
     model = BinaryClassifier(args.input_features, args.hidden_dim, args.output_dim)
     model.to(device)
     
@@ -149,7 +144,7 @@ if __name__ == '__main__':
     # Trains the model (given line of code, which calls the above training function)
     train(model, train_loader, args.epochs, criterion, optimizer, device)
 
-    ## TODO: complete in the model_info by adding three argument names, the first is given
+
     # Keep the keys of this dictionary as they are 
     model_info_path = os.path.join(args.model_dir, 'model_info.pth')
     with open(model_info_path, 'wb') as f:
@@ -160,7 +155,6 @@ if __name__ == '__main__':
         }
         torch.save(model_info, f)
         
-    ## --- End of your code  --- ##
     
 
 	# Save the model parameters
